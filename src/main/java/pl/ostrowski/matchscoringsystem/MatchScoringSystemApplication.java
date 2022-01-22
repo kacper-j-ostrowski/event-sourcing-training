a6a19af029cca63327f65431b67f1a6be920f2a0
Kacper Ostrowski <kacper.j.ostrowski@gmail.com>
2022-01-22 16:10:17 +0100

    spring boot boilerplate

---
+++ src/main/java/pl/ostrowski/matchscoringsystem/MatchScoringSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.ostrowski.matchscoringsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchScoringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchScoringSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/ostrowski/matchscoringsystem/MatchScoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ostrowski.matchscoringsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchScoringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
